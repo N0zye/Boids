1f02aba220153131cb795ff8dff4b196ffb83215
N0zye <michele.berni@gmail.com>
2024-03-12 17:49:42 +0100

    May brake everything after this, last commit before 3D scaling

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
-#include <raylib.h>
 #include "boid.h"
+#include <raylib.h>
 
-int WinMain() {
+int WinMain()
+{
 
-	InitWindow(1024, 512, "Boids");
-	SetTargetFPS(144);
-	BoidGroup bg(300);
-	while (!WindowShouldClose()){
-		bg.update();
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawFPS(GetScreenWidth()-90, 10);
-		bg.draw();
-		EndDrawing();
-	}
+    InitWindow(1024, 512, "Boids");
+    SetTargetFPS(60);
 
-	CloseWindow();
-	return 0;
+    // Define the camera to look into our 3d world
+    Camera camera = { 0 };
+    camera.position = Vector3 { 0.0f, 10.0f, 10.0f };
+    camera.target = Vector3 { 0.0f, 0.0f, 0.0f };
+    camera.up = Vector3 { 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    BoidGroup bg(300);
+    while (!WindowShouldClose()) {
+        bg.update();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+        bg.draw();
+        EndMode3D();
+
+        DrawFPS(GetScreenWidth() - 90, 10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
